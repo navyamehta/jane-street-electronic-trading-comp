e45e6288e6b2b61ac161c578b7f0b04f55d6189c
Navya Mehta <45111560+navyamehta@users.noreply.github.com>
2019-06-08 11:47:07 -0400

    Updated liquidity methods

--- bondtrade.py
+++ bondtrade.py
@@ -2,19 +2,27 @@
 #acceptable limit and also have buy orders above that. In that regard, both for loops will not run in the code
 #under traditional market circumstances
 def bondtrade(sellprc, sellamt, buyprc, buyamt, curhold):
-	shortlim = -100
-	longlim = 100
-	accepprc = 1000
-	sellamt = sellamt[sellprc < accepprc]
-	sellprc = sellprc[sellprc < accepprc]
-	buyamt = buyamt[buyprc > accepprc]
-	buyprc = buyprc[buyprc > accepprc]
-	orders = []
-	#We check how . many current sell orders are below fair value, and how many buys are above
-	for i in range(0, len(sellamt)):
-		amt = min(sellamt[i], abs(longlim-curhold))
-		orders.append(['buy', sellprc[i], amt])
-	for i in range(0, len(buyamt)):
-		amt = min(buyamt[i], abs(shortlim-curhold))
-		orders.append(['sell', buyprc[i], amt])
-	return orders
+    accepprc = 1000
+    sellamt = sellamt[sellprc < accepprc]
+    sellprc = sellprc[sellprc < accepprc]
+    buyamt = buyamt[buyprc > accepprc]
+    buyprc = buyprc[buyprc > accepprc]
+    newbuyval = 0
+    newsellval = 0
+    for i in range(0, len(sellamt)):
+        newbuyval += sellprc[i] * sellamt[i]
+    for i in range(0, len(buyamt)):
+        newsellval += buyprc[i] * buyamt[i]
+    shortlim = -65
+    longlim = 65
+    orders = np.array([])
+    for i in range(0, len(sellamt)):
+        amt = min(sellamt[i], abs(longlim-curhold))
+        amt = max(0, amt)
+        orders = np.append(orders, np.array([['buy', sellprc[i], amt]])).reshape(-3,3)
+    #Irrational orders clear automatically i.e. you cant have buy above acceplim and sell below acceplim
+    for i in range(0, len(buyamt)):
+        amt = min(buyamt[i], abs(shortlim-curhold))
+        amt = max(0, amt)
+        orders = np.append(orders, np.array([['sell', buyprc[i], amt]])).reshape(-3,3)
+    return [orders, newbuyval, newsellval]
