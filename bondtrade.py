b2befc6558d9130cf8acfe805c4a58310d71569b
Navya Mehta <45111560+navyamehta@users.noreply.github.com>
2019-06-08 12:21:03 -0400

    Update bondtrade.py

--- bondtrade.py
+++ bondtrade.py
@@ -1,7 +1,8 @@
-#This strategy assumes that the market behaves rationally and thus you cannot have sell orders below an arbitrary
-#acceptable limit and also have buy orders above. Thus, this code currently does not run against itself, and instead
-#only reads the best clearing price in the market. It executes orders to perfectly match amounts on the list as
-#it does not currently have cancel functionality
+#In addition to the old bond strategy earlier, we also add functionality to place cursory orders that are ideally profitable.
+#However, if a better price is detected, we nullify said orders and improve market standing
+import numpy as np
+import pandas as pd
+
 def bondtrade(sellprc, sellamt, buyprc, buyamt, curhold, fill=0):
     accepprc = 1000
     shortlim = -65
