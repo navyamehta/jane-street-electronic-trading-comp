94da2dad7c4710ca31a3d182876bd756378cdd1e
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-06-08 13:19:53 -0400

    working bond

--- bondtrade.py
+++ bondtrade.py
@@ -1,20 +1,55 @@
-#This strategy assumes that the market behaves rationally and thus you cannot have sell orders below an arbitrary
-#acceptable limit and also have buy orders above that. In that regard, both for loops will not run in the code
-#under traditional market circumstances
-def bondtrade(sellprc, sellamt, buyprc, buyamt, curhold):
-	shortlim = -100
-	longlim = 100
-	accepprc = 1000
-	sellamt = sellamt[sellprc < accepprc]
-	sellprc = sellprc[sellprc < accepprc]
-	buyamt = buyamt[buyprc > accepprc]
-	buyprc = buyprc[buyprc > accepprc]
-	orders = []
-	#We check how . many current sell orders are below fair value, and how many buys are above
-	for i in range(0, len(sellamt)):
-		amt = min(sellamt[i], abs(longlim-curhold))
-		orders.append(['buy', sellprc[i], amt])
-	for i in range(0, len(buyamt)):
-		amt = min(buyamt[i], abs(shortlim-curhold))
-		orders.append(['sell', buyprc[i], amt])
-	return orders
+#In addition to the old bond strategy earlier, we also add functionality to place cursory orders that are ideally profitable.
+#However, if a better price is detected, we nullify said orders and improve market standing
+import numpy as np
+import pandas as pd
+
+def bondtrade(sellprc, sellamt, buyprc, buyamt, curhold, incbuy, incsell, fill=0):
+    accepprc = 1000
+    shortlim = -80
+    longlim = 80
+    orders = np.array([])
+    if len(sellprc) == 0:
+        statsell = 1002
+    else:
+        statsell = max(1002, min(sellprc) - 1)
+    if len(buyprc) == 0:
+        statbuy = 998
+    else:
+        statbuy = min(998, max(buyprc) + 1)
+    sellamt = sellamt[sellprc < accepprc]
+    sellprc = sellprc[sellprc < accepprc]
+    buyamt = buyamt[buyprc > accepprc]
+    buyprc = buyprc[buyprc > accepprc]
+    newbuyval = 0
+    newsellval = 0
+    for i in range(0, len(sellamt)):
+        newbuyval += sellprc[i] * sellamt[i]
+    newbuyval += statbuy * 10
+    for i in range(0, len(buyamt)):
+        newsellval += buyprc[i] * buyamt[i]
+    newsellval += statsell * 10
+    #We only need to nullify an inefficient old buy order, since if a sell order is below the lowest buy, it is still executed
+    #by the marketplace at aforementioned buy price
+    if isinstance(fill, list) & (len(sellprc) > 0):
+        cnc = cancel(sellprc, sellamt, curhold, fill)
+        orders = np.append(orders, cnc).reshape(-3,3)
+    for i in range(0, len(sellamt)):
+        amt = min(sellamt[i], abs(longlim-curhold))
+        amt = max(0, amt)
+        orders = np.append(orders, np.array([['buy', sellprc[i], amt]])).reshape(-3,3)
+    for i in range(0, len(buyamt)):
+        amt = min(buyamt[i], abs(shortlim-curhold))
+        amt = max(0, amt)
+        orders = np.append(orders, np.array([['sell', buyprc[i], amt]])).reshape(-3,3)
+    if (curhold+incbuy)<(longlim-1):
+        orders = np.append(orders, np.array([['buy', statbuy, 1]])).reshape(-3,3)
+    if (curhold-incsell) > (shortlim+1):
+        orders = np.append(orders, np.array([['sell', statsell, 1]])).reshape(-3,3)
+    return [orders, newbuyval, newsellval]
+
+
+def cancel(prc, amt, hold, fill):
+    if fill[0] < min(prc):
+        return np.array([])
+    else:
+        return np.array(['cancel', fill[0], fill[1]])
--- client.py
+++ client.py
@@ -2,18 +2,21 @@
 
 # ~~~~~==============   HOW TO RUN   ==============~~~~~
 # 1) Configure things in CONFIGURATION section
-# 2) Change permissions: chmod +x bot.py
-# 3) Run in loop: while true; do ./bot.py; sleep 1; done
+# 2) Change permissions: chmod +x client.py
+# 3) Run in loop: while true; do ./client.py; sleep 1; done
 
 from __future__ import print_function
 
 import sys
 import socket
 import json
+import time
+import numpy as np
+from bondtrade import bondtrade
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
-team_name="Pani Puri Street"
+team_name="panipuristreet"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
 test_mode = True
@@ -22,7 +25,7 @@ test_mode = True
 # 0 is prod-like
 # 1 is slower
 # 2 is empty
-test_exchange_index=2
+test_exchange_index=0
 prod_exchange_hostname="production"
 
 port=25000 + (test_exchange_index if test_mode else 0)
@@ -43,16 +46,114 @@ def read_from_exchange(exchange):
 
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
+# rm client.py && nano client.py
 
 def main():
     exchange = connect()
     write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
-    hello_from_exchange = read_from_exchange(exchange)
-    # A common mistake people make is to call write_to_exchange() > 1
-    # time for every read_from_exchange() response.
-    # Since many write messages generate marketdata, this will cause an
-    # exponential explosion in pending messages. Please, don't do that!
-    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+
+    # buy_order = {"type": "add", "order_id": 5, "symbol": "BOND", "dir": "BUY", "price": 1000, "size": 20}
+    # write_to_exchange(exchange, buy_order)
+    # [[1001, 7], [1002, 2]]
+
+    current_bonds = 0
+
+    order_id = 1
+    TIMEOUT = 5
+
+    total_orders_buy = 0
+    success_orders_buy = 0
+
+    total_orders_sell = 0
+    success_orders_sell = 0
+
+    to_send = []
+    working_id = 0
+    trying = False
+    while True:
+        try:
+            res_from_exchange = read_from_exchange(exchange)
+        except:
+            continue
+        # print("The exchange replied:", res_from_exchange, file=sys.stderr)
+        # print("\n")
+
+        res_type = res_from_exchange["type"]
+        if res_type == "hello":
+            symbols = res_from_exchange["symbols"]
+            for symbol in symbols:
+                if symbol["symbol"] == "BOND":
+                    current_bonds = symbol["position"]
+                    print(current_bonds)
+                    time.sleep(TIMEOUT)
+        elif res_type == "open":
+            pass
+        elif res_type == "book":
+            symbol = res_from_exchange["symbol"]
+
+            if symbol == "BOND":
+
+                buy_res= res_from_exchange["buy"]
+                sell_res = res_from_exchange["sell"]
+
+                buy_prices = []
+                buy_amounts = []
+
+                sell_prices = []
+                sell_amounts = []
+
+                for order in buy_res:
+                    buy_prices.append(order[0])
+                    buy_amounts.append(order[1])
+
+                for order in sell_res:
+                    sell_prices.append(order[0])
+                    sell_amounts.append(order[1])
+
+                order_data = bondtrade(np.array(sell_prices), np.array(sell_amounts), np.array(buy_prices), np.array(buy_amounts), current_bonds, total_orders_buy-success_orders_buy, total_orders_sell-success_orders_sell)
+                orders = order_data[0].tolist()
+
+                build = {}
+                # print("# Orders (OURS): ")
+                # print(len(orders))
+                for order in orders:
+                    if order[0] == "buy":
+                        build = {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", "price": int(order[1]), "size": int(order[2])}
+                        total_orders_buy += 1
+                    elif order[0] == "sell":
+                        build = {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "SELL", "price": int(order[1]), "size": int(order[2])}
+                        total_orders_sell += 1
+                    order_id += 1
+                    print(build)
+                    to_send.append(build)
+        elif res_type == "fill":
+            if res_from_exchange["dir"] == "buy":
+                success_orders_buy += 1
+            elif res_from_exchange["dir"] == "sell":
+                success_orders_sell += 1
+            print(res_from_exchange)
+            time.sleep(TIMEOUT)
+        elif res_type == "reject":
+            print(res_from_exchange)
+            time.sleep(TIMEOUT)
+        elif res_type == "error":
+            print(res_from_exchange)
+            time.sleep(TIMEOUT)
+        elif res_type == "ack":
+            print(res_from_exchange)
+            working_id += 1
+            trying = False
+            time.sleep(TIMEOUT)
+
+        if working_id == 0:
+            if len(to_send) > 0 and not trying:
+                write_to_exchange(exchange, to_send[working_id])
+                trying = True
+        else:
+            if not trying:
+                write_to_exchange(exchange, to_send[working_id])
+                trying = True
+
 
 if __name__ == "__main__":
     main()
